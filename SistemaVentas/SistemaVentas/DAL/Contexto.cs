d9438d3cc53af3abd41d8900eb06496c3d79b975
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-16 07:58:31 -0400

    Add project files.

---
+++ SistemaVentas/SistemaVentas/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SistemaVentas.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+        
+    }
+}
