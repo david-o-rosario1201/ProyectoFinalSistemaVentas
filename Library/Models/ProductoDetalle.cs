c2acb6eae3e99f300453b6916951161dfad297e5
Stiinger <ortegantony29@gmail.com>
2024-03-16 18:48:41 -0400

    Crear ProductoDetalle.

--- Library/Models/ProductoDetalle.cs
+++ Library/Models/ProductoDetalle.cs
@@ -8,7 +8,7 @@ public class ProductoDetalle
 	public int ProductoDetalleId { get; set; }
 	public int ProveedorId { get; set; }
 	public int ProductoId { get; set; }
-	[Range(1, 100000, ErrorMessage = "El campo {0} debe ser mayor que 0 y menor que 100000.")]
+	[Range(0, 100000, ErrorMessage = "El campo {0} debe ser mayor que 0 y menor que 100000.")]
 	public float Costo { get; set; }
 	public bool Eliminado { get; set; } = false;
 }
