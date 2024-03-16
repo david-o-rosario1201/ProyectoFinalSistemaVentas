85a3b9e25ac08a3ab1ec0ca37f61508a450a51fb
Stiinger <ortegantony29@gmail.com>
2024-03-16 18:48:22 -0400

    Crear modelo Categorias.

---
+++ Library/Models/Categorias.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Models;
+
+public class Categorias
+{
+	[Key]
+	public int CategoriaId { get; set; }
+	[Required(ErrorMessage = "Este campo es obligatorio.")]
+	public string Descripcion { get; set; } = string.Empty;
+	[ForeignKey("CategoriaId")]
+	public ICollection<Productos> Productos { get; set; } = new List<Productos>();
+}
