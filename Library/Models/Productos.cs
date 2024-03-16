8f5241337b1cf51faf4716079a3fd9a09e774725
Stiinger <ortegantony29@gmail.com>
2024-03-16 19:47:25 -0400

    Agregar campo existencia.

--- Library/Models/Productos.cs
+++ Library/Models/Productos.cs
@@ -19,7 +19,7 @@ public class Productos
 	public int CategoriaId { get; set; }
 	[Range(0, 100000, ErrorMessage = "El campo {0} debe ser mayor que 0 y menor que 100000.")]
 	public float Precio { get; set; }
-
+	public int Existencia { get; set; }
 	public bool Eliminado { get; set; } = false;
     public string Nota { get; set; } = string.Empty;
 
