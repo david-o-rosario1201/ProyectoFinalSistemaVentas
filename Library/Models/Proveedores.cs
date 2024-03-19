2d58feb2123a5d62aaada1ce44d24f56724cc424
Stiinger <ortegantony29@gmail.com>
2024-03-18 23:52:37 -0400

    Rename

--- Library/Models/Proveedores.cs
+++ Library/Models/Proveedores.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Shared.Models;
+namespace Library.Models;
 
 public class Proveedores
 {
--- SistemaVentas/SistemaVentas/DAL/Contexto.cs
+++ SistemaVentas/SistemaVentas/DAL/Contexto.cs
@@ -1,4 +1,4 @@
-﻿using Shared.Models;
+﻿using Library.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace SistemaVentas.DAL;
