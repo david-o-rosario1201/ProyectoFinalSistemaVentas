c6d454e00f247045a1a8529606ba7ada75302609
Stiinger <ortegantony29@gmail.com>
2024-03-18 23:52:37 -0400

    Rename

--- Library/Models/ProductoDetalle.cs
+++ Library/Models/ProductoDetalle.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Shared.Models;
+namespace Library.Models;
 
 public class ProductoDetalle
 {
--- Library/Models/Productos.cs
+++ Library/Models/Productos.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Shared.Models;
+namespace Library.Models;
 
 public class Productos
 {
