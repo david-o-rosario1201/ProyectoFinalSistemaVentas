c498870213b44e4850eb2de909fe4518aa2998f1
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-23 15:58:24 -0400

    Se agregaron los services

---
+++ SistemaVentas.Services/Services/ClientesService.cs
@@ -0,0 +1,24 @@
+﻿using SistemaVentas.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentas.Services.Services;
+
+public class ClientesService
+{
+    private readonly HttpClient _httpClient;
+
+    public ClientesService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Clientes>> GetClientes()
+    {
+        return await _httpClient.GetFromJsonAsync<List<Clientes>>("api/Clientes");
+    }
+}
