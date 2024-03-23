19d9239949e75922139540e4a01b96953777816e
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-23 15:58:53 -0400

    Se agregó el service Proveedor.

---
+++ SistemaVentas.Services.Client/Services/ProveedoresService.cs
@@ -0,0 +1,11 @@
+﻿using SistemaVentas.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentas.Services.Services;
+
+
