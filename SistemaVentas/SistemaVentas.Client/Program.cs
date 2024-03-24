67c8b5bb54e09945df48e3002a83d22afe0e23e7
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-23 20:48:55 -0400

    Agregué un método que contiene la URI.

--- SistemaVentas/SistemaVentas.Client/Program.cs
+++ SistemaVentas/SistemaVentas.Client/Program.cs
@@ -3,4 +3,13 @@ using SistemaVentas.Services.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+
+builder.Services.AddScoped(http => new HttpClient
+{
+	BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
+
+builder.Services.AddScoped<ClientesService>();
+builder.Services.AddScoped<ProveedoresService>();
+
 await builder.Build().RunAsync();
