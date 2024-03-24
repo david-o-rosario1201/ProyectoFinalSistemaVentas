55adf4d8d2f845618aa6e6b7883faf8f168ce781
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-24 12:49:21 -0400

    Services del lado del cliente listos hasta ahora 24/03/2024

--- SistemaVentas.Services/Services/ProveedoresService.cs
+++ SistemaVentas.Services/Services/ProveedoresService.cs
@@ -3,40 +3,45 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using System;
+using System.Linq;
+using System.Text;
+
 
 namespace SistemaVentas.Services.Services;
 
 public class ProveedoresService
 {
-    private readonly HttpClient _httpClient;
+	private readonly HttpClient _httpClient;
 
-    public ProveedoresService(HttpClient httpClient)
-    {
-        _httpClient = httpClient;
-    }
+	public ProveedoresService(HttpClient httpClient)
+	{
+		_httpClient = httpClient;
+	}
 
-    public async Task<List<Proveedores>> GetProveedores()
-    {
-        return await _httpClient.GetFromJsonAsync<List<Proveedores>>("api/Proveedores");
-    }
+	public async Task<List<Proveedores>> GetProveedores()
+	{
+		var proveedores = await _httpClient.GetFromJsonAsync<List<Proveedores>>("api/Proveedores");
+		return proveedores;
+	}
 
-    public async Task<Proveedores> GetProveedor(int id)
-    {
-        return await _httpClient.GetFromJsonAsync<Proveedores>($"api/Proveedores/{id}");
-    }
+	public async Task<Proveedores> GetProveedor(int id)
+	{
+		return await _httpClient.GetFromJsonAsync<Proveedores>($"api/Proveedores/{id}");
+	}
 
-    public async Task<HttpResponseMessage> Save(Proveedores proveedor)
-    {
-        return await _httpClient.PostAsJsonAsync("api/Proveedores", proveedor);
-    }
+	public async Task<HttpResponseMessage> Save(Proveedores proveedor)
+	{
+		return await _httpClient.PostAsJsonAsync("api/Proveedores", proveedor);
+	}
 
-    public async Task<HttpResponseMessage> Update(Proveedores proveedor)
-    {
-        return await _httpClient.PutAsJsonAsync($"api/Proveedores", proveedor);
-    }
+	public async Task<HttpResponseMessage> Update(Proveedores proveedor)
+	{
+		return await _httpClient.PutAsJsonAsync($"api/Proveedores", proveedor);
+	}
 
-    public async Task<HttpResponseMessage> Delete(int id)
-    {
-        return await _httpClient.DeleteAsync($"api/Proveedores/{id}");
-    }
+	public async Task<HttpResponseMessage> Delete(int id)
+	{
+		return await _httpClient.DeleteAsync($"api/Proveedores/{id}");
+	}
 }
---
+++ SistemaVentas.Services/Services/TiposContribuyenteService.cs
@@ -0,0 +1,33 @@
+﻿using SistemaVentas.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVentas.Services.Services;
+
+public class TiposContribuyenteService
+{
+	private readonly HttpClient _httpClient;
+
+	public TiposContribuyenteService(HttpClient httpClient)
+	{
+		_httpClient = httpClient;
+	}
+
+	public async Task<List<TiposContribuyente>> GetTiposContribuyentes()
+	{
+		return await _httpClient.GetFromJsonAsync<List<TiposContribuyente>>("api/TiposContribuyentes");
+	}
+}
